31813eaf91aad33ecc308e2db3a2e16f4ed110f1
FernandoDD01 <fealvarado07@gmail.com>
2022-10-03 04:20:55 -0500

    agregué conexion con firebase y modifiqué la pagina de gacetas

--- src/js/code.js
+++ src/js/code.js
@@ -1,4 +1,57 @@
-const url = "http://localhost:3000/api/gacetas/";
+let contenedor = document.querySelector("tbody");
+let resultados = "";
+
+//Credenciales para acceder a la base de datos de firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBj0omjzGZ0VGWVgANO4qvUfZFO3EQQg1U",
+  authDomain: "decanato-ciiemad-ipn.firebaseapp.com",
+  projectId: "decanato-ciiemad-ipn",
+  storageBucket: "decanato-ciiemad-ipn.appspot.com",
+  messagingSenderId: "239098004211",
+  appId: "1:239098004211:web:43650b37aa3786c4622d86",
+  measurementId: "G-BQDKX7BKGY",
+  // ...
+};
+
+// Inicializar firebase
+firebase.initializeApp(firebaseConfig);
+// Inicializar Cloud Firestore
+const db = firebase.firestore();
+
+//acceder a la tabla de gacetas y formar los renglones de la tabla
+db.collection("gacetas")
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((gacetas) => {
+      resultados += `
+                    <tr class="text-center">
+                    <td>${gacetas.id}</td>
+                    <td>${gacetas.data().documento_historico}</td>
+                    <td>${gacetas.data().numero}</td>
+            
+                    <td>
+                    <a href="assets/pdf_gacetas/${
+                      gacetas.data().numero
+                    }.pdf" target="_blank">
+                    <img src="assets/port_gacetas//${
+                      gacetas.data().imagen
+                    }" alt="${
+        gacetas.data().numero
+      }" width="70px" height="90px">
+                    </a>
+                    </td>
+                    </tr>`;
+    });
+    contenedor.innerHTML = resultados;
+  });
+
+// Initialize Cloud Storage and get a reference to the service
+
+// Get a non-default Storage bucket
+
+//CODIGO PARA CONSUMIR API que acceda a la base de datos
+
+/*const url = "http://localhost:3000/api/gacetas/";
 const contenedor = document.querySelector("tbody");
 let resultados = "";
 
@@ -25,4 +78,4 @@ const mostrar = (gacetas) => {
 fetch(url)
   .then((response) => response.json())
   .then((data) => mostrar(data))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(error)); */
