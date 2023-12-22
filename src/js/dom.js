70b5c1e9e2483a772842f08934051f3947bb6bf1
FernandoDD01 <fealvarado07@gmail.com>
2023-12-22 00:58:50 -0600

    Se terminó de estilizar el navegador

--- src/js/dom.js
+++ src/js/dom.js
@@ -2,131 +2,204 @@ const $header = document.querySelector("header"),
   $main = document.querySelector("main"),
   $footer = document.querySelector("footer");
 
-/*$header.innerHTML = ``;*/
+$header.innerHTML = ``;
 
-$main.innerHTML = ` <!--Contenedor Carrusel-->
-<div 
-  id="carouselExampleIndicators"
-  class="carousel slide "
-  data-bs-ride="true"
-  style="z-index: 1"
-  
->
-  <div class="carousel-indicators">
-    <buttons
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="0"
-      class="active"
-      aria-current="true"
-      aria-label="Slide 1"
-    ></buttons>
-    <button
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="1"
-      aria-label="Slide 2"
-    ></button>
-    <button
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="2"
-      aria-label="Slide 3"
-    ></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img
-        src="assets/imagenes/Banner_REDAALC.jpg"
-        class="d-block w-100"
-        alt="aviso1"
-      />
-    </div>
-    <div class="carousel-item">
-      <img
-        src="assets/imagenes/Banner_CIMA_2023.jpg"
-        class="d-block w-100"
-        alt="aviso2"
-      />
-    </div>
-    <div class="carousel-item">
-      <img
-        src="assets/imagenes/ciiemad_50dmma.jpg"
-        class="d-block w-100"
-        alt="aviso3"
-      />
-    </div>
-  </div>
-  <button
-    class="carousel-control-prev"
-    type="button"
-    data-bs-target="#carouselExampleIndicators"
-    data-bs-slide="prev"
-  >
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button
-    class="carousel-control-next"
-    type="button"
-    data-bs-target="#carouselExampleIndicators"
-    data-bs-slide="next"
-  >
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<!--Espacio abajo del carrusel-->
-<div class="bg-light">
-  <div class="abajo-Carrusel"></div>
-</div>
+$header.innerHTML += `
+<!--navegador-->
 
-<div class="bg-IPN">
-  <div class="contenedorGinda"></div>
-</div>
+      <nav class="navbar navbar-expand-lg navbar-dark contenedorNavegador">
+        <div class="container-fluid justify-content-center">
+         
+        <div class="banderin">
+        <img
+          class="banderin-vacio"
+          src="assets/imagenes/banderin.png"
+          width="100"
+          height="100"
+          alt="banderin-vacio"
+          class="banderin-img justify-content-left"
+        />
+        <img
+          class="escudo-ciiemad"
+          src="assets/imagenes/banderin-ciiemad.png"
+          width="75"
+          height="75"
+          alt="escudo-ciiemad"
+          class="banderin-img justify-content-left"
+        />
+      </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="true"
+            aria-label="Toggle navigation"
+          >
+            <a class="navbar-brand" href="#">Menú Principal</a>
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-`;
+          <div
+            class="collapse navbar-collapse justify-content-center"
+            id="navbarNavDropdown"
+            style="margin-top: 0px"
+          >
 
-/*$footer.innerHTML = `
+         
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a
+                  href="index.html"
+                  class="nav-link active"
+                  aria-current="page"
+                  title="Inicio"
+                  >Inicio</a
+                >
+              </li>
+              <li class="nav-item">
+                <a href="CV_decana.html" class="nav-link" title="CV-decana"
+                  >Decana</a
+                >
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  title="Archivo-Historico"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  >Archivo Histórico</a
+                >
+                <ul class="dropdown-menu">
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="Hechos_historicos.html"
+                      title="Hechos historicos"
+                      ><div class="selected" style="display: none"></div>
+                      <p>Hechos Históricos</p>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="Gacetas.html"
+                      title="Gacetas"
+                    >
+                      Gacetas</a
+                    >
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a
+                  href="Historial_del_CIIEMAD.html"
+                  class="nav-link"
+                  title="Hechos_historicos_IPN"
+                  >Historia del CIIEMAD</a
+                >
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  title="Personas_destacadas"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  >Personas destacadas</a
+                >
+                <ul class="dropdown-menu color-black">
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="Profesores.html"
+                      title="Profesores"
+                    >
+                      Profesores</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="Alumnos.html"
+                      title="Alumnos"
+                    >
+                      Alumnos</a
+                    >
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  title="Galería"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  >Galería</a
+                >
+                <ul class="dropdown-menu color-black">
+                  <li>
+                    <a class="dropdown-item" href="Fotos.html" title="Fotos">
+                      Fotos</a
+                    >
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="Videos.html" title="Videos">
+                      Videos</a
+                    >
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div
+          id="contenedorAccesibilidad-scroll"
+          class="contenedorAccesibilidad u-oculto"
+        ></div>
+      </nav>
 
+      <!--Contenedor Nombre Instituto-->
+      
 
+      `;
 
-`;*/
+if (document.querySelector("body").classList.contains("root")) {
+  $header.innerHTML += `<div class="contenedorEscuela">Decanato CIIEMAD IPN</div>`;
+}
 
-/*<div> Hola</div>
-
-<div class="container text-center">
-<div class="row">
-  <div class="col-2">
+$footer.innerHTML = ` <div class="footer-guinda"></div>
+<div class="footer-gray">
+  <div class="footer-escudo-ipn">
     <a href="https://www.ipn.mx/" target="_blank">
       <img
-        class="img-fluid"
         src="/assets/imagenes/Logo_IPN.png"
-        alt="Logo_IPN"
-        width="120"
-        height="240"
+        alt="escudo-IPN"
+        width="100"
+        height="100"
       />
     </a>
   </div>
-  <div class="col-8">
-    <ul class="list-unstyled small text-muted text-center">
-      <li class="mb-2 fw-bolder">
-        © Todos los derechos reservados - Fernando Enrique Alvarado
-        Bolaños - Septiembre 2023
-      </li>
 
-      <li class="mb-2 fw-bolder">Versión 1 . 0 . 0</li>
-    </ul>
+  <div class="footer-autor">
+    <p>© Todos los derechos reservados</p>
+    <p>- Fernando Enrique Alvarado Bolaños -</p>
+    <p>Enero 2024</p>
   </div>
-  <div class="col-2">
+
+  <div class="footer-escudo-ciiemad">
     <a href="https://www.ciiemad.ipn.mx/" target="_blank"
       ><img
-        class="img-fluid logo-ciiemad"
         src="/assets/imagenes/banderin-ciiemad.png"
-        alt="Logo_IPN"
-        width="120"
-        height="240"
+        alt="escudo-ciiemad"
+        width="100"
+        height="100"
     /></a>
   </div>
-</div>
-</div>*/
+</div>`;
